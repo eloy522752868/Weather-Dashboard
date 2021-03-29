3434d5e94746ce3b39209234e4135c380ceabcc6
Eloy Gonzalez <eloy522752@gmail.com>
2021-03-29 10:37:59 -0400

    started adding js script pull the api

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,22 @@
      //   alert("Yeah!");
   //  }
 //}
+
+
+var searchFormEl = document.querySelector('#search-form');
+var searchCityEl = document.querySelector('#search-city');
+var todayContainerEl = document.querySelector('#today-container');
+var fiveDayEl = document.querySelector('#five-day-container');
+
+
+searchFormEl.on("submit", function(event)
+{
+    event.preventDefault();
+    var searchCity = searchCityEl.val();
+    console.log(searchCity);
+    var apiKey = "19b818be73dc1ae04217d6a6cfb8c6db"
+    var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q= " +
+    searchCity + "&appid=" + apiKey + " ";
+
+});
+
