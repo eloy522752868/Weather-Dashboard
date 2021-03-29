c57f0883600c3b21194e3c1008942b449c24bd84
Eloy Gonzalez <eloy522752@gmail.com>
2021-03-28 23:41:56 -0400

    added files and starting adding html and css.

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+//alert("HellowWrold")
+
+
+//window.onload = function() {
+    //if ('undefined' == typeof window.jQuery) { 
+        // jQuery is loaded  
+        //alert("Doesn't Work");
+   // } else {
+        // jQuery is not loaded
+      
+     //   alert("Yeah!");
+  //  }
+//}
